d367d2b8e8c3fd3679e2afe96a5da8dc1022b383
izMystic <mystic@izmystic.dev>
2021-05-27 10:55:22 -0400

    change config format

--- Mystic-RPCommands/config.lua
+++ Mystic-RPCommands/config.lua
@@ -1,9 +1,9 @@
-CONFIG = {
-    ME_COLOR = '#000080', -- HEX COLOR FOR /ME
-    DO_COLOR ='#DA14FF', -- HEX COLOR FOR /DO
-    GME_COLOR = '#39FF14', -- HEX COLOR FOR /GME
-    TWT_COLOR = '#1DA1F2', -- HEX COLOR FOR /TWT
-    OOC_COLOR = '#FFA500', -- HEX COLOR FOR /OOC
-    DARKWEB_COLOR = '#000000', -- HEX COLOR FOR /DARKWEB
-    ADVERT_COLOR = '#FFDA14' -- HEX COLOR FOR /AD
+Config = {
+    Me-Color = '#000080', -- HEX COLOR FOR /ME
+    Do-Color ='#DA14FF', -- HEX COLOR FOR /DO
+    GME-Color = '#39FF14', -- HEX COLOR FOR /GME
+    TWT-Color = '#1DA1F2', -- HEX COLOR FOR /TWT
+    OOC-Color = '#FFA500', -- HEX COLOR FOR /OOC
+    Darkweb-Color = '#000000', -- HEX COLOR FOR /DARKWEB
+    Advert-Color = '#FFDA14' -- HEX COLOR FOR /AD
 }

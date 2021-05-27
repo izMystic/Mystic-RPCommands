31b59a5fcf0d00ee515f1177f96e5811dffa9668
izMystic <mystic@izmystic.dev>
2021-05-27 13:29:11 -0400

    Update config.lua

--- Mystic-RPCommands/config.lua
+++ Mystic-RPCommands/config.lua
@@ -1,6 +1,6 @@
 Config = {}
     Config.Me-Color = '#000080', -- HEX COLOR FOR /ME
-    Config.Do-Color ='#DA14FF', -- HEX COLOR FOR /DO
+    Config.Do-Color = '#DA14FF', -- HEX COLOR FOR /DO
     Config.GME-Color = '#39FF14', -- HEX COLOR FOR /GME
     Config.TWT-Color = '#1DA1F2', -- HEX COLOR FOR /TWT
     Config.OOC-Color = '#FFA500', -- HEX COLOR FOR /OOC
